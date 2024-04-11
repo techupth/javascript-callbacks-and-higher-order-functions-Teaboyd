14f0564660b7f89aa67d9026c199eac85ca79ba4
Teaboyd <ph.patiphan@kkumail.com>
2024-04-11 17:59:41 +0700

    complete ex 1 and learning ex 2

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,19 @@
 
 function forEach(array, operation) {
   // Start coding here
+  return array+operation;
 }
+
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
+function digitalwallet5000(add5K,arr,num){
+  for (let i = 0; i<arr.length; i++){
+    newEmployeeSalaries.push(add5K(arr[i],num));
+  }
+  return newEmployeeSalaries;
+}
 // Using `forEach` function here
-
+const patiphan = digitalwallet5000(forEach,employeeSalaries,5000);
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,24 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  return array(operation);
+}
+
+function zixboyd(fork,song,funk){
+  const conditon = [];
+  let takeoff;
+  for (let i = 0;i < song.length; i++){
+    if (i === 0){
+      fork[i] > 50;
+      takeoff = song[i];
+      console.log (`นักเรียนห้องที่ ${funk} ผ่านเกณฑ์ ✅`);
+    }else if (takeoff < song[i]){
+      song[i] < 50;
+      takeoff = song[i];
+      console.log (`นักเรียนห้องที่ ${funk} ไม่ผ่านเกณฑ์ ❌`);
+    }
+    return conditon;
+  }
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +28,6 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = zixboyd(studentScoresRoom1,atLeastFive);
+let scoreRoom2Result = zixboyd(studentScoresRoom2,atLeastFive);
+let scoreRoom3Result = zixboyd(studentScoresRoom3,atLeastFive);
