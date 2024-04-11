33cda526907a65b79f068593551e71376a333bc0
Teaboyd <ph.patiphan@kkumail.com>
2024-04-11 18:16:27 +0700

    fix ex 1_2 and learning something new

--- ex-1.js
+++ ex-1.js
@@ -9,12 +9,13 @@ function forEach(array, operation) {
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
-function digitalwallet5000(add5K,arr,num){
+function digitalwallet5000(add5K,arr,podduang){
   for (let i = 0; i<arr.length; i++){
-    newEmployeeSalaries.push(add5K(arr[i],num));
+    newEmployeeSalaries.push(add5K(arr[i],podduang));
   }
   return newEmployeeSalaries;
 }
 // Using `forEach` function here
-const patiphan = digitalwallet5000(forEach,employeeSalaries,5000);
+const betterHappinessFrom5000 = digitalwallet5000(forEach,employeeSalaries,5000);
+console.log(betterHappinessFrom5000);
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
